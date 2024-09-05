9276c90e7ecbe63f0db52dd754a05d9a4487facc
matheusvirissimo <matheus.virissimo@unesp.br>
2024-09-05 16:11:09 -0300

    feat: adição da função split para futura inserção

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include <stdbool.h>
 
 #define MAX 10
-#define t 2 // o "t" é dinâmico. Por enquanto só testes
-
+#define t 2 // o "t" é dinâmico. Por enquanto só testes. "T" é um valor de controle para quantidades de chaves
 typedef struct no{
     int n; // quantidade de chaves
     int chave[MAX]; // números contidos na chaves
@@ -12,18 +11,14 @@ typedef struct no{
     struct no *filho[MAX+1] // apontando para o próximo
 } ArvB;
 
-typedef struct btree{
-    ArvB* raiz;
-} BTree;
-
 
 // criar nó na árvore
-ArvB* criarNoArvoreB(BTree* T){
+ArvB* criarNoArvoreB(ArvB** raiz){
     ArvB* x = (ArvB*) malloc(sizeof(ArvB)); // aloca no 
     x->folha = true; // o nó é folha
     x->n = 0; // inicia sem nenhuma chave
     escrever(x); // escreve no disco binário
-    T->raiz = x; // o novo nó vai ser a raiz
+    *raiz = x; // o novo nó vai ser a raiz
     return x; // retorna o novo nó
 }
 
@@ -52,6 +47,37 @@ ArvB* buscaArvoreB(ArvB* arvore, int numeroBuscado){ // k é a chave que buscamo
 
 // split de um nó;
 void splitChildrenArvoreB(ArvB* arvore, int i){
+    ArvB* z = (ArvB*) malloc(sizeof(ArvB)); // aloca memória para o nó z;
+    // ArvB* y = (ArvB*) malloc(sizeof(ArvB)); não precisa dar malloc no "z" porque dá vazamento de memória se fizer isso
+
+    ArvB* y = arvore->filho[i]; // y vai ser o i-ésimo filho desse nó
+    z->folha = y->folha; // z vai receber se y é folha (ou não)
+
+    z->n = t - 1; // a quantidade de  chaves de z será t-1 (por que?)
+
+    for(int j = 0; j <= t - 1; j++){ // enquanto não alcançarmos o mínimo de chaves possíveis
+        z->chave[j] = y->chave[j+t]; // as chaves de "z" serão as últimas t-1 chaves de y
+    }
+
+    if(y->folha == false){ // se "y" não for folha
+        for(int j = 0; j <= t; j++){ // enquanto j não for t
+            z->filho[j] = y->filho[j+t]; // "z" vai receber os filhos de "y" em ordem crescente
+        }
+    }
+
+    y->n = t-1; // "y" terá a quantidade mínima de chaves possíveis
+    
+    // agora fazemos o split da direita
+    for(int j = (arvore->n + 1); j >= i + 1; j--){ // enquanto não alcançarmos o começo da chave
+        arvore->filho[j+1] - arvore->filho[j];
+    }
+
+    arvore->chave[i] = y->chave[t]; // a mediana das chaves sobe para o nó-pai
+    arvore->n = arvore->n + 1; // o número de chaves em x aumenta em 1
+
+    escrever(arvore);
+    escrever(y);
+    escrever(z);
 
 }
 
